2f216bbf35e0daa5ef440ee0b0688e00bcf5a65f
Kirill Starodubsev <colereteps@gmail.com>
2022-12-28 00:13:53 +0400

    added chat page

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require("express")
-const path = require("path")
-const DEFAULT_PORT = 3000
+const express = require('express');
+const path = require('path');
+const DEFAULT_PORT = 3000;
 
-const app = express() //initialization
+const app = express(); // Initialization
 // const __dirname = path.resolve() //current path
-const PORT = process.env.PORT || DEFAULT_PORT
+const PORT = process.env.PORT || DEFAULT_PORT;
 
-app.use(express.static(path.join(__dirname + "/dist")))
+app.use(express.static(path.join(__dirname + '/dist')));
 
-app.get("/profile", (req, res) => {
-  res.sendFile(__dirname + "/dist/pages/profile/profile.html")
-})
-app.get("/*", (req, res) => {
-  res.sendFile(__dirname + "/dist/pages/404/404.html")
-})
+app.get('/chat', (req, res) => {
+	res.sendFile(__dirname + '/dist/pages/chat/chat.html');
+});
+app.get('/*', (req, res) => {
+	res.sendFile(__dirname + '/dist/pages/404/404.html');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}`)
-})
+	console.log(`Server has been started on port ${PORT}`);
+});
